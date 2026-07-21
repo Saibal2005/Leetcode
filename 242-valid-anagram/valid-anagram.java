c501b363c1c341205bd59770a7a34fe1836be835
Saibal2005 <duttabiswajit326@gmail.com>
2026-07-21 23:52:11 +0530

    Time: 5 ms (61.47%) | Memory: 44.3 MB (75.10%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -5,15 +5,15 @@ class Solution {
             return false;
         }
 
-        int[] count = new int[26];
+        int[] freq = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
         }
 
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
+        for (int count : freq) {
+            if (count != 0) {
                 return false;
             }
         }
