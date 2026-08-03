d500edd4a65e3df8dbe8cc34ef9493d2d5d5469b
Saibal2005 <duttabiswajit326@gmail.com>
2026-08-04 01:49:58 +0530

    Time: 1 ms (59.06%) | Memory: 49 MB (9.38%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,35 +1,45 @@
 class Solution {
     public int trap(int[] height) {
-
-        int left = 0;
-        int right = height.length - 1;
-
-        int leftMax = 0;
-        int rightMax = 0;
-
-        int water = 0;
-
-        while (left < right) {
-
-            if (height[left] <= height[right]) {
-
-                if (height[left] >= leftMax)
-                    leftMax = height[left];
-                else
-                    water += leftMax - height[left];
-
-                left++;
-            } else {
-
-                if (height[right] >= rightMax)
-                    rightMax = height[right];
-                else
-                    water += rightMax - height[right];
-
-                right--;
+        int [] lmax=new int[height.length];
+        int [] rmax=new int[height.length];
+        int k=0,l=0;
+        int sum=0;
+        for(int i=0;i<height.length;i++)
+        {
+            if(height[i]>k)
+            {
+                lmax[i]=height[i];
+                k=height[i];
+            }
+            else
+            {
+                lmax[i]=k;
             }
         }
-
-        return water;
+        for(int i=height.length-1;i>=0;i--)
+        {
+            if(height[i]>l)
+            {
+                rmax[i]=height[i];
+                l=height[i];
+            }
+            else
+            {
+                rmax[i]=l;
+            }
+            
+        }
+        for(int i=0;i<height.length;i++)
+        {
+                if(lmax[i]>rmax[i])
+                {
+                    sum=sum+rmax[i]-height[i];
+                }
+                else
+                {
+                    sum=sum+lmax[i]-height[i];
+                }
+        }
+        return sum;
     }
 }
